4b1f9a18a3fea13d6e605d6b6a7dd9d593944c9d
jsansbury <james@sprocketcreative.com>
2009-05-01 16:23:47 -0400

    Worked on admin screen.

--- region_manager.js
+++ region_manager.js
@@ -1,1 +1,21 @@
 // $Id$
+
+$(function() {
+  // Block administration page.
+  $('#region-manager-blocks-form tr.module input').change(function() {
+    var classes = $(this).parents('td').getClassNames();
+    // build our selector from the classes
+    var selector = 'td.' + classes.join('.');
+    // disable the checkboxes depending on whether the checkbox was checked or not.
+    $(selector + ' input').not('[id*=all-blocks]').attr('disabled', $(this).is(':checked'));
+  });
+});
+
+$.fn.getClassNames = function() {
+  if (name = this.attr("class")) {
+    return name.split(" ");
+  }
+  else {
+    return [];
+  }
+}
