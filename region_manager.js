1a79c58e07d63a8a1179d63a5c3236fc62f1f690
q0rban <q0rban@MacBook-Pro.local>
2009-05-02 22:45:53 -0400

    Reworking the admin UI.

--- region_manager.js
+++ region_manager.js
@@ -2,14 +2,50 @@
 
 $(function() {
   // Block administration page.
-  $('#region-manager-blocks-form tr.module input').change(function() {
+  var selects = $('#region-manager-blocks-form tr.module select');
+  selects.change(function() {
+    var module = $(this).parents('tr').getClassNames().slice(0, 1);
+    showHideRows(module);
+
     var classes = $(this).parents('td').getClassNames();
     // build our selector from the classes
     var selector = 'td.' + classes.join('.');
-    // disable the checkboxes depending on whether the checkbox was checked or not.
-    $(selector + ' input').not('[id*=all-blocks]').attr('disabled', $(this).is(':checked'));
+    blockCheckboxes(this.value, selector);
+
   });
+  selects.change();
 });
+
+showHideRows = function(module) {
+  var show = 0;
+  $('#region-manager-blocks-form tr.module.' + module + ' select').each(function(i) {
+    if (this.value == 2) {
+      show = 1;
+    }
+  });
+  if (show) {
+    $('#region-manager-blocks-form tr[class^=' + module + ']').not('tr.' + module).show();
+  }
+  else {
+    $('#region-manager-blocks-form tr[class^=' + module + ']').not('tr.' + module).hide();
+  }
+}
+
+blockCheckboxes = function(value, selector) {
+  switch(value) {
+    case '0':
+      $(selector + ' input').not('[id*=all-blocks]').attr('checked', 0);
+      break;
+
+    case '1':
+      $(selector + ' input').not('[id*=all-blocks]').attr('checked', 1);
+      break;
+
+    case '2':
+      $(selector + ' input').not('[id*=all-blocks]').attr('disabled', 0);
+      break;
+  }
+}
 
 $.fn.getClassNames = function() {
   if (name = this.attr("class")) {
