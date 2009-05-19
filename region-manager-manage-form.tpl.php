756d1ee8ca21d4b34075f8d6b35eabbe6505a609
jsansbury <james@sprocketcreative.com>
2009-05-19 11:10:05 -0400

    Working towards a redesign of the Region Manager UI.

--- region-manager-manage-form.tpl.php
+++ region-manager-manage-form.tpl.php
@@ -1,18 +1,18 @@
 <?php
-// $Id: block-admin-display-form.tpl.php,v 1.3 2008/01/16 22:57:26 goba Exp $
+// $Id: region-manager-manage-form.tpl.php,v 1.1.2.1 2009/05/18 01:38:26 q0rban Exp $
 
 /**
  * @file block-admin-display-form.tpl.php
  * Default theme implementation to configure blocks.
  *
  * Available variables:
- * - $block_regions: An array of regions. Keyed by name with the title as value.
+ * - $block_states: An array of states. Keyed by name with the title as value.
  * - $block_listing: An array of blocks keyed by region and then delta.
  * - $form_submit: Form submit button.
  *
- * Each $block_listing[$region] contains an array of blocks for that region.
+ * Each $block_listing[$state] contains an array of blocks for that region.
  *
- * Each $data in $block_listing[$region] contains:
+ * Each $data in $block_listing[$state] contains:
  * - $data->region_title: Region title for the listed block.
  * - $data->block_title: Block title.
  * - $data->region_select: Drop-down menu for assigning a region.
@@ -30,27 +30,49 @@
   drupal_add_js('misc/tableheader.js');
   drupal_add_js(drupal_get_path('module', 'region_manager') .'/region_manager.js');
   drupal_add_js(array('block_name' => _region_manager_block_name()), 'setting');
-  foreach ($block_regions as $region => $title) {
-    drupal_add_tabledrag('blocks', 'match', 'sibling', 'block-region-select', 'block-region-'. $region, NULL, FALSE);
-    drupal_add_tabledrag('blocks', 'order', 'sibling', 'block-weight', 'block-weight-'. $region);
+  foreach ($block_states as $state => $title) {
+    //drupal_add_tabledrag('blocks', 'match', 'sibling', 'block-region-select', 'block-region-'. $region, NULL, FALSE);
+    drupal_add_tabledrag('region-manager-blocks-active-path', 'order', 'sibling', 'block-weight', 'block-weight-'. RM_BLOCK_ACTIVE_PATH);
   }
 ?>
-<table id="blocks" class="sticky-enabled">
+<table id="region-manager-blocks-active-path">
   <tbody>
     <?php $row = 0; ?>
-    <?php foreach ($block_regions as $region => $title): ?>
-      <tr class="region region-<?php print $region?>">
-        <td colspan="4" class="region"><?php print $title; ?></td>
+    <tr class="rm-state rm-state-<?php print RM_BLOCK_ACTIVE_PATH ?>">
+      <td colspan="4" class="rm-state"><?php print $block_states[RM_BLOCK_ACTIVE_PATH] ?></td>
+    </tr>
+    <tr class="rm-state-message rm-state-<?php print $state?>-message <?php print empty($block_listing[$state]) ? 'rm-state-empty' : 'rm-state-populated'; ?>">
+      <td colspan="4"><em><?php print t('No @blocks', array('@block' => _region_manager_block_name())); ?></em></td>
+    </tr>
+    <?php foreach ($block_listing[RM_BLOCK_ACTIVE_PATH] as $delta => $data): ?>
+    <tr class="rm-block rm-block-<?php print $delta; ?> rm-state-<?php print RM_BLOCK_ACTIVE_PATH ?> draggable <?php print $row % 2 == 0 ? 'odd' : 'even'; ?><?php print $data->row_class ? ' '. $data->row_class : ''; ?>">
+      <td class="block"><?php print $data->block_title; ?></td>
+      <td><?php print $data->region_select; ?></td>
+      <td><?php print $data->weight_select; ?></td>
+      <td class="operations"><?php print $data->operations; ?></td>
+    </tr>
+    <?php $row++; ?>
+    <?php endforeach; ?>
+    <?php unset($block_states[RM_BLOCK_ACTIVE_PATH]); ?>
+  </tbody>
+</table>
+
+<table id="region-manager-blocks-available">
+  <tbody>
+    <?php $row = 0; ?>
+    <?php foreach ($block_states as $state => $title): ?>
+      <tr class="rm-state rm-state-<?php print $state ?>">
+        <td colspan="4" class="rm-state"><?php print $title ?></td>
       </tr>
-      <tr class="region-message region-<?php print $region?>-message <?php print empty($block_listing[$region]) ? 'region-empty' : 'region-populated'; ?>">
+      <tr class="rm-state-message rm-state-<?php print $state?>-message <?php print empty($block_listing[$state]) ? 'rm-state-empty' : 'rm-state-populated'; ?>">
         <td colspan="4"><em><?php print t('No @blocks', array('@block' => _region_manager_block_name())); ?></em></td>
       </tr>
-      <?php foreach ($block_listing[$region] as $delta => $data): ?>
-      <tr class="<?php print $row % 2 == 0 ? 'odd' : 'even'; ?><?php print $data->row_class ? ' '. $data->row_class : ''; ?>">
+      <?php foreach ($block_listing[$state] as $delta => $data): ?>
+      <tr class="rm-block rm-block-<?php print $delta; ?> rm-state-<?php print $state ?> <?php print $row % 2 == 0 ? 'odd' : 'even'; ?><?php print $data->row_class ? ' '. $data->row_class : ''; ?>">
         <td class="block"><?php print $data->block_title; ?></td>
         <td><?php print $data->region_select; ?></td>
         <td><?php print $data->weight_select; ?></td>
-        <td class="title-link"><?php print $data->title_link; ?></td>
+        <td class="operations"><?php print $data->operations; ?></td>
       </tr>
       <?php $row++; ?>
       <?php endforeach; ?>
