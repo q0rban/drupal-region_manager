1115f2fda6ff4df72e058298fe31d1147cadeff6
jsansbury <james@sprocketcreative.com>
2009-05-26 15:36:17 -0400

    Latest styling from sprocketjared.

--- region_manager.js
+++ region_manager.js
@@ -1,4 +1,4 @@
-// $Id: region_manager.js,v 1.1.2.4 2009/05/21 02:50:13 q0rban Exp $
+// $Id: region_manager.js,v 1.1.2.5 2009/05/21 16:59:23 q0rban Exp $
 
 /**
  * Show/hide form elements for editing a block title.
